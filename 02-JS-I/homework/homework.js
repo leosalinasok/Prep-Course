d1ad4b16f4220a1d73210f3acbc55f954270b9da
leosalinasok <leonardonahuel.salinas@gmail.com>
2022-06-18 04:31:36 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = null;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = hola;
 
 // Crea una variable booleana:
 const nuevoBool = null;
@@ -15,7 +15,7 @@ const nuevaResta = 10 - null === 5;
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
-// Resuelve el siguiente problema matemático:
+// Resuelve el siguiente problema matesmático:
 const nuevoModulo = 21 % 5 === null;
 
 
